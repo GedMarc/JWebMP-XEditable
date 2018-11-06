b1953fc06ecbce127cccf96e8a0dfb9f6ef168ec
GedMarc <ged_marc@hotmail.com>
2018-11-06 21:29:33 +0200

    JRE 11 full split and module specifications a bit stricter

---
+++ src/main/java/com/jwebmp/plugins/xeditable/implementations/XEditableExclusionsModule.java
@@ -0,0 +1,30 @@
+package com.jwebmp.plugins.xeditable.implementations;
+
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
+
+import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Set;
+
+public class XEditableExclusionsModule
+		implements IGuiceScanModuleExclusions<XEditableExclusionsModule>,
+				           IGuiceScanJarExclusions<XEditableExclusionsModule>
+{
+
+	@Override
+	public @NotNull Set<String> excludeJars()
+	{
+		Set<String> strings = new HashSet<>();
+		strings.add("jwebmp-xeditable-*");
+		return strings;
+	}
+
+	@Override
+	public @NotNull Set<String> excludeModules()
+	{
+		Set<String> strings = new HashSet<>();
+		strings.add("com.jwebmp.plugins.xeditable");
+		return strings;
+	}
+}
