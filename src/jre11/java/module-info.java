37ca900cbd32d4bdf3bad97b8c0e85a4e3b581e5
GedMarc <ged_marc@hotmail.com>
2018-11-25 21:28:07 +0200

    Updates for Jackson

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -16,12 +16,13 @@ module com.jwebmp.plugins.xeditable {
 	provides IGuiceScanJarExclusions with XEditableExclusionsModule;
 
 	requires com.jwebmp.core;
-	requires com.fasterxml.jackson.annotation;
+	requires transitive com.fasterxml.jackson.annotation;
 
 	requires java.logging;
 	requires com.jwebmp.plugins.jqueryui;
 	requires java.validation;
 	requires com.jwebmp.guicedinjection;
+	requires com.fasterxml.jackson.databind;
 
 	opens com.jwebmp.plugins.xeditable to com.fasterxml.jackson.databind, com.jwebmp.core;
 	opens com.jwebmp.plugins.xeditable.options to com.fasterxml.jackson.databind, com.jwebmp.core;
