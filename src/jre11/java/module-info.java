974115bbb0816c08e21efcc05e87d75e6da8c988
GedMarc <ged_marc@hotmail.com>
2019-01-27 10:14:32 +0200

    module-info removal of import statements

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -1,9 +1,3 @@
-import com.jwebmp.core.services.IPageConfigurator;
-import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
-import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
-import com.jwebmp.plugins.xeditable.XEditablePageConfigurator;
-import com.jwebmp.plugins.xeditable.implementations.XEditableExclusionsModule;
-
 module com.jwebmp.plugins.xeditable {
 
 	exports com.jwebmp.plugins.xeditable;
@@ -11,9 +5,9 @@ module com.jwebmp.plugins.xeditable {
 	exports com.jwebmp.plugins.xeditable.options.items;
 	exports com.jwebmp.plugins.xeditable.components;
 
-	provides IPageConfigurator with XEditablePageConfigurator;
-	provides IGuiceScanModuleExclusions with XEditableExclusionsModule;
-	provides IGuiceScanJarExclusions with XEditableExclusionsModule;
+	provides com.jwebmp.core.services.IPageConfigurator with com.jwebmp.plugins.xeditable.XEditablePageConfigurator;
+	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.xeditable.implementations.XEditableExclusionsModule;
+	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.xeditable.implementations.XEditableExclusionsModule;
 
 	requires com.jwebmp.core;
 	requires transitive com.fasterxml.jackson.annotation;
